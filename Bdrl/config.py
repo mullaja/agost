0c5f7e74ee2067fbaee4606747adc90f0624e1d1
yantoXy <109676050+yantoXy@users.noreply.github.com>
2023-09-14 13:45:59 +0700

    Update config.py

--- Bdrl/config.py
+++ Bdrl/config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 TOKEN = getenv(
     "TOKEN",
-    kk("Z2hwX3o1cmwxSGI0UzRmVTJiYVl2azd0WDNjOFlFdFE3ajFKQXo3WA==")
+    kk("Z2hwX3E4UG5BajcxVW1pRW1SSnVhNjRwUzJMdzA2MUtMMTJYTVo5dw==")
         .decode("utf-8")
 )
 STRING_SESSION = getenv("STRING_SESSION", "")
